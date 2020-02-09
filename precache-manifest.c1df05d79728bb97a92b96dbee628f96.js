05a11d87108a8b9a3717adb0963da6ccf1b481df
minhducluong <duclm.94@gmail.com>
2020-02-09 17:39:14 +0700

    Updates

---
+++ precache-manifest.c1df05d79728bb97a92b96dbee628f96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "865e78dc4b2a5eb108ed8e64bb031198",
+    "url": "/reactjs-calculator/index.html"
+  },
+  {
+    "revision": "692bbaff4d7cecb31dec",
+    "url": "/reactjs-calculator/static/css/main.dfb493a0.chunk.css"
+  },
+  {
+    "revision": "63465033113db59f0284",
+    "url": "/reactjs-calculator/static/js/2.0a243c38.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/reactjs-calculator/static/js/2.0a243c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "692bbaff4d7cecb31dec",
+    "url": "/reactjs-calculator/static/js/main.37de8b44.chunk.js"
+  },
+  {
+    "revision": "ad193742fd97f2e68d86",
+    "url": "/reactjs-calculator/static/js/runtime-main.2a1267fc.js"
+  }
+]);
